965e5688a7a57df8d3147442029f1a2730958795
Leo Marqz <leomarqz2020@gmail.com>
2022-12-21 21:09:28 -0600

    implementacion de test unitarios en proceso ...

---
+++ MoviesWebApi.Tests/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MoviesWebApi.Helpers;
+using NetTopologySuite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesWebApi.Tests
+{
+    public class TestBase
+    {
+        //retorna un ApplicationDbContext con el cual podremos 
+        //crear una base de datos por cada prueba sin perjudicar 
+        //la base de datos original.
+        protected ApplicationDbContext CreateContext(string DBName)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(DBName).Options;
+            var dbContext = new ApplicationDbContext(options);
+            return dbContext;
+        }
+
+        //retorna una instancia de automapper con todas las configuraciones de nuestro 
+        //automapper en MoviesWebApi
+        protected IMapper ConfigureAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfiles(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ MoviesWebApi.Tests/UnitTests/GenresControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesWebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesWebApi.Tests.UnitTests
+{
+    [TestClass]
+    public class GenresControllerTests : TestBase
+    {
+        [TestMethod]
+        public async Task GetAllGenres()
+        {
+            //Preparation
+            var DbName = Guid.NewGuid().ToString();
+            var context = CreateContext(DbName);
+            var mapper = ConfigureAutoMapper();
+            context.Genres.Add(new Entities.Genre { Name = "Genero 1" });
+            context.Genres.Add(new Entities.Genre { Name = "Genero 2" });
+            await context.SaveChangesAsync();
+
+            var context2 = CreateContext(DbName);
+
+            //Test
+            var controller = new GenresController(context2, mapper);
+            var response = await controller.Get();
+
+            //Verification
+            var genres = response.Value;
+            Assert.AreEqual(2, genres.Count);
+
+        }
+
+        [TestMethod]
+        public async Task GetGenreByIdNotExists()
+        {
+            //Preparation
+            var DbName = Guid.NewGuid().ToString();
+            var context = CreateContext(DbName);
+            var mapper = ConfigureAutoMapper();
+            
+            //Test
+            var controller = new GenresController(context, mapper);
+            var response = await controller.Get(1);
+            var result = response.Result as StatusCodeResult;
+
+            //Verification
+            Assert.AreEqual(404, result.StatusCode);
+        }
+    }
+}
